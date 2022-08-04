9e1491f9521c4c68fea935a0e62ec366c419df64
Amanda Meneghin <amanda.meneghin@hadrion.com.br>
2022-08-03 23:39:22 -0300

    refazendo layout dos cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Modal from "./Modal";
 import { FaHeart } from "react-icons/fa";
 import { useFavorite } from '../contexts/contextFavorites';
+import fundoLivro from "../assets/img/capa-livro-fundo.png"
 
 
 const Card = ({ book }) => {
@@ -16,15 +17,24 @@ const Card = ({ book }) => {
     if (thumbnail !== undefined && amount !== undefined) {
         return (
             <>
-                <div className="card" >
-                    <img className="thumbnail" onClick={() => { setShow(true); setItem(book) }} src={thumbnail} alt="" /><br />
-                    <div className="bottom">
-                        <h5 className="title">{book.volumeInfo.title}</h5>
-                        {/* <p className="amount"></p> */}
-                        <br />
+                <div className='card-livro'>
+                    <img className="thumbnail" onClick={() => { setShow(true); setItem(book) }} src={thumbnail} alt="" />
+                    <div className='descricao'>
+                        <b><h5 className="title">{book.volumeInfo.title}</h5></b>
+                        <h6 className="title">
+                            {book.volumeInfo.authors.map(txt => <p>{txt}</p>)}
+                        </h6><br />
                         <button onClick={() => { toggleFavorite(book) }} className={"favorito " + (isFavorite ? "favoritado" : "")}><FaHeart></FaHeart></button>
                     </div>
                 </div>
+                {/* <div className="card" >
+                    <img className="thumbnail" onClick={() => { setShow(true); setItem(book) }} src={thumbnail} alt="" /><br />
+                    <div className="bottom">
+                        <h5 className="title">{book.volumeInfo.title}</h5>
+                        <br />
+                        
+                    </div>
+                </div> */}
                 <Modal show={show} item={bookItem} onClose={() => setShow(false)} />
             </>
         )
