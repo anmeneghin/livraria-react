910afbf5a16825f15fbbc175fbdc17e320ad6420
Amanda Meneghin <amanda.meneghin@hadrion.com.br>
2022-08-31 22:04:04 -0300

    links nos contatos | ajustes layout

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -32,8 +32,11 @@ function Contacts() {
                     <h1>Entre em contato!</h1>
                     <h4 className='descricao-contatos'>Lorem ipsum dolor sit, consectetur adipiscing elit. Nunc vulputate volutpat purus vel efficitur. Sed efficitur lacus et urna pulvinar, vel eleifend nisl dignissim.</h4>
                     <br />
-                    <h3 className='icons-social-media'><FaInstagram></FaInstagram>&nbsp; <FaLinkedin></FaLinkedin>&nbsp;
-                        <FaGithub></FaGithub></h3>
+                    <h3 className='icons-social-media'>
+                        <a target="_blank" href='https://www.instagram.com/anmeneghin/' rel="noreferrer"><FaInstagram></FaInstagram>&nbsp;</a>
+                        <a target="_blank" href='https://www.linkedin.com/in/anmeneghin/' rel="noreferrer"><FaLinkedin></FaLinkedin>&nbsp;</a>
+                        <a target="_blank" href='https://github.com/anmeneghin/' rel="noreferrer"><FaGithub></FaGithub></a>
+                    </h3>
                 </div>
                 <img className="imagemFundo" src={ImagemFundo} alt="fundo" />
             </div>
